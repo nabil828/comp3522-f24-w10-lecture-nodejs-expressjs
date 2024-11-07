60b7bd9ffd3f61bf1d136051e2b082171603f83c
Nabil <rousani@gmail.com>
2024-11-07 09:49:25 -0800

    sendFile

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const path = require('path');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./home.html"))
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.get("/aboutus", (req, res) => {
+  res.send("<h1> Welcome to the about us page!</h1>")
+})
+
+app.get("/page1", (req, res) => {
+  res.send("<h1> Welcome to page1!</h1>")
+})
+
+// app.put()
+// app.delete()
+// app.post()
+
+app.listen(3000, () => {
+  console.log("Successfully listening to port 3000");
 })
